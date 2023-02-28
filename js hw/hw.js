4404d0bc12e45642487ad16068628f72fa0a4d7d
smuushi <mishih780@gmail.com>
2023-02-27 23:56:55 -0500

    js hw...

---
+++ js hw/hw.js
@@ -0,0 +1,13 @@
+// function madLib(verb, adj, noun) {
+//     return 'We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.' 
+
+// }
+function madLib(verb, adj, noun) {
+    return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`
+  }
+
+madLib('make', 'best', 'guac');
+
+
+
+// i swear I tried to make this work, but it's not working..
